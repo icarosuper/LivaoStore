6c4e9b8130efa7f20d5168e3cfa3293bbb749617
icarosuper <icarogomesmotta@gmail.com>
2026-06-01 17:39:12 -0300

    chore: alterar favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,7 +9,7 @@ import { TRPCReactProvider } from "~/trpc/react";
 export const metadata: Metadata = {
 	title: "Doces da Kiki",
 	description: "Doces artesanais com encanto de bruxa",
-	icons: [{ rel: "icon", url: "/favicon.ico" }],
+	icons: [{ rel: "icon", url: "/favicon.png" }],
 };
 
 const baloo = Baloo_2({
